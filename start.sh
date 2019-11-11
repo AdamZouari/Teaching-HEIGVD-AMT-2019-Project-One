0c0da83d25132ac9f49794f019508091869c0d08
Adam <adam.zouari@heig-vd.ch>
2019-11-11 22:40:37 +0100

    modifiy script

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ echo "Environnement $1 is running ....."
 mvn clean install -q -Dmaven.test.skip=true
 
 if [ $1 = "prod" ]; then
-	mv target/projectone.war images/payara/
+	mv target/projectone.war docker/images/payara/
 fi
 cd docker/topologies/chillout-${1} && docker-compose down
 docker-compose up --build && cd ../../
